6b10792eb9a7ac89d4b1aa8463510f32688dda16
ndean87 <ndean87@gmail.com>
2017-10-15 10:15:19 -0400

    Done.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 $(document).ready(function() {
-
-  lightActive();
-  redLight();
-  yellowLight();
-  greenLight();
-
-  
+	lightActive();
+	redLight();
+	yellowLight();
+	greenLight();
 });
 
-function lightActive(){
-  $('.light').on('click', function(){
-      $(this).toggleClass("active");
-      console.log("background purple")
-  });
+function lightActive() {
+	$('.light').on('click', function() {
+		$(this).toggleClass('active');
+		console.log('background purple');
+	});
 }
 
-function redLight(){
-  $(".redLight").on('click', function(){
-    $(this).toggleClass("red");
-  });
+function redLight() {
+	$('.redLight').on('click', function(event) {
+		$(this).toggleClass('red');
+		event.stopPropagation();
+	});
 }
 
-
-function yellowLight(){
-  $(".yellowLight").on('click', function(){
-    $(this).toggleClass("yellow");
-  });
+function yellowLight() {
+	$('.yellowLight').on('click', function(event) {
+		$(this).toggleClass('yellow');
+		event.stopPropagation();
+	});
 }
 
-function greenLight(){
-  $(".greenLight").on('click', function(){
-    $(this).toggleClass("green");
-  });
+function greenLight() {
+	$('.greenLight').on('click', function(event) {
+		$(this).toggleClass('green');
+		event.stopPropagation();
+	});
 }
-
